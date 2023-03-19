1f733a2ec8a4efdaedd36f9079ed4ca50e500fa1
noman17rus <noman171991@gmail.com>
2023-03-19 13:38:03 +0400

    add MyArrayList (collection)

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,22 @@
-import homeWork.Person;
+import test.MyArrayList;
 
 public class Main {
     public static void main(String[] args) {
+        MyArrayList emloyees = getEmployees();
+        emloyees.add("James");
+        emloyees.remove("Emma");
+        for (int i = 0; i < emloyees.getSize(); i++) {
+            System.out.println(emloyees.get(i));
+        }
+    }
+
+    private static MyArrayList getEmployees() {
+        MyArrayList employees = new MyArrayList();
+        employees.add("John");
+        employees.add("Olivia");
+        employees.add("Emma");
+        employees.add("Max");
+        employees.add("Nick");
+        return employees;
     }
 }
